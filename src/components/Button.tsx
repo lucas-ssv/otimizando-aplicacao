d6836341bc1a1cefcdd95b8581c0f10528cfe13e
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-09 09:57:52 -0300

    Challenge optimizing application finished

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,26 @@
-import { Icon } from './Icon';
+import { Icon } from "./Icon";
 
-import '../styles/button.scss';
-import { ButtonHTMLAttributes } from 'react';
+import "../styles/button.scss";
+import { ButtonHTMLAttributes, useMemo } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
-  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  iconName: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
   selected: boolean;
 }
 
 export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
+  const memorizedIcon = useMemo(() => {
+    return <Icon name={iconName} color={selected ? "#FAE800" : "#FBFBFB"} />;
+  }, [iconName, selected]);
+
   return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+    <button
+      type="button"
+      {...(selected && { className: "selected" })}
+      {...rest}
+    >
+      {memorizedIcon}
       {title}
     </button>
   );
